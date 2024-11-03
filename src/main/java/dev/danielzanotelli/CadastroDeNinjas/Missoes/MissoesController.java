ee5dc291aeefbcb9d55a7fbd1d66ee57b6a05ca7
Daniel Zanotelli <danielzanotelli12@gmail.com>
2024-11-03 19:31:44 -0300

    refactor: refatora o controller de missoes

--- src/main/java/dev/danielzanotelli/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/danielzanotelli/CadastroDeNinjas/Missoes/MissoesController.java
@@ -3,33 +3,31 @@ package dev.danielzanotelli.CadastroDeNinjas.Missoes;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
     //ADICIONAR MISSÃO (CREATE)
-    @PostMapping("/cria_missao")
+    @PostMapping("/criar")
     public String criarMissao(){
         return "Criar missão";
     }
     //SELECIONAR TODAS AS MISSÕES(READ)
-    @GetMapping("/todas_missoes")
-    public String selecionarTodasAsMissoes() {
+    @GetMapping("/listar")
+    public String listarMissoes() {
         return "Selecionar todas as missões";
     }
-    //SELECIONAR MISSÃO POR ID(READ)
-    @GetMapping("/missaoID")
-    public String selecionarMissaoPorId(){
-        return "Selecionar missão por id";
-    }
+
     //ALTERAR MISSÃO POR ID(UPDATE)
-    @PutMapping("/alterar_missaoID")
-    public String alterarMissaoPorId(){
-        return "Alterar missao por id";
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Alterar missao";
     }
     //DELETAR MISSÃO(DELETE)
-    @DeleteMapping("/deletar_missaoID")
-    public String deletarMissaoPorId(){
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
         return "Deletar missão por id";
     }
+
+
 
 }
