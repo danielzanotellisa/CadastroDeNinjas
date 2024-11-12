21c7be5ac83943fcb57b2f32611aa575e9bf6fbe
Daniel Zanotelli <danielzanotelli12@gmail.com>
2024-11-12 13:14:51 -0300

    chore: Adiciona as camadas de abstração de NinjaDTO e NinjaMapper

--- src/main/java/dev/danielzanotelli/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/danielzanotelli/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.danielzanotelli.CadastroDeNinjas.Ninjas;
 
+import dev.danielzanotelli.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+
+    private String email;
+    private Integer idade;
+    private MissoesModel missao;
+    private String rank;
 }
--- src/main/java/dev/danielzanotelli/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/danielzanotelli/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,31 @@
 package dev.danielzanotelli.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissao(ninjaDTO.getMissao());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissao(ninjaModel.getMissao());
+
+        return ninjaDTO;
+    }
 }
