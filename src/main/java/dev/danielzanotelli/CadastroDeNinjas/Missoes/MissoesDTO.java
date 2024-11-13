d9655150253b693a7abedf9f70a839ccc9dd8982
Daniel Zanotelli <danielzanotelli12@gmail.com>
2024-11-13 10:36:04 -0300

    chore: Faz o mapeamento do meu model e DTO

--- src/main/java/dev/danielzanotelli/CadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/danielzanotelli/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -1,4 +1,18 @@
 package dev.danielzanotelli.CadastroDeNinjas.Missoes;
 
+
+import dev.danielzanotelli.CadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesDTO {
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private List<NinjaModel> ninjas;
 }
